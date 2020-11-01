cf8201099ff9162ff2dd181845b69cccf11570ba
newfrenchy83 <31768074+newfrenchy83@users.noreply.github.com>
2020-11-01 20:39:35 +0100

    Update udisplay.cpp

--- src/units/udisplay.cpp
+++ src/units/udisplay.cpp
@@ -675,12 +675,12 @@ void unit_attack(display * disp, game_board & board,
 					continue;
 				}
 
-		unit_map::const_iterator leader = board.units().find(ability.teacher_loc);
-		assert(leader.valid());
-		leader->set_facing(ability.teacher_loc.get_relative_dir(a));
-		animator.add_animation(leader.get_shared_ptr(), "leading", ability.teacher_loc,
-			att->get_location(), damage, true,  "", {0,0,0},
-			hit_type, weapon, secondary_attack, swing);
+				unit_map::const_iterator leader = board.units().find(ability.teacher_loc);
+				assert(leader.valid());
+				leader->set_facing(ability.teacher_loc.get_relative_dir(a));
+				animator.add_animation(leader.get_shared_ptr(), "leading", ability.teacher_loc,
+				att->get_location(), damage, true,  "", {0,0,0},
+				hit_type, weapon, secondary_attack, swing);
 			}
 		}
 	}
